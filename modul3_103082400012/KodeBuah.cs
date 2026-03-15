158d6a41fa80ca8bd602800c99c71648ceed8b9c
fdhill <muhammadfadhilahnaf11@gmail.com>
2026-03-15 12:56:02 +0800

    menambahkan kelas kodebuah

---
+++ modul3_103082400012/KodeBuah.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul3_103082400012
+{
+    public class KodeBuah
+    {
+        private Dictionary<String, String> tabelBuah = new Dictionary<String, String>()
+        {
+            {"Apel", "A00"},
+            {"Aprikot", "B00"},
+            {"Alpukat", "C00"},
+            {"Pisang", "D00"},
+            {"Paprika", "E00"},
+            {"Blackberry", "F00"},
+            {"Ceri", "H00"},
+            {"Kelapa", "I00"},
+            {"Jagung", "J00"},
+            {"Kurma", "K00"},
+            {"Durian", "L00"},
+            {"Anggur", "M00"},
+            {"Melon", "N00"},
+            {"Semangka", "O00"}
+        };
+
+        public String getKodeBuah (String namaBuah)
+        {
+            return tabelBuah[namaBuah];
+        }
+    }
+}
